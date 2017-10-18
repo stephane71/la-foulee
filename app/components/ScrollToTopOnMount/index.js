df59b4496267874b660b34f6f9c947f19b5b057a
Stephane Maire <stephanemaire71@gmail.com>
2017-10-18 13:18:40 +0200

    ScrollToTopOnMount | create component

---
+++ app/components/ScrollToTopOnMount/index.js
@@ -0,0 +1,22 @@
+/**
+*
+* ScrollToTopOnMount
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+class ScrollToTopOnMount extends React.Component {
+
+  componentWillMount () {
+    window.scrollTo(0, 0)
+  }
+
+  render () {
+    return null
+  }
+}
+
+export default ScrollToTopOnMount;
