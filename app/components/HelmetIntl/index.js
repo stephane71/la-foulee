966332c120a25305292510a2d03469fdde8267b0
Stephane Maire <stephanemaire71@gmail.com>
2017-10-18 12:49:24 +0200

    HelmetIntl | create component Manage title & meta tag translations with injectIntl HOC

---
+++ app/components/HelmetIntl/index.js
@@ -0,0 +1,27 @@
+/**
+*
+* HelmetIntl
+*
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { injectIntl } from 'react-intl';
+
+
+function HelmetIntl({ intl, title, content }) {
+  return (
+    <Helmet>
+      <title>{intl.formatMessage(title)}</title>
+      <meta name={`description`} content={intl.formatMessage(content)} />
+    </Helmet>
+  );
+}
+
+HelmetIntl.propTypes = {
+  title: PropTypes.object.isRequired,
+  content: PropTypes.object.isRequired
+};
+
+export default injectIntl(HelmetIntl);
