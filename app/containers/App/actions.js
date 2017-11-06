78579fb92d629cadfab1fb0f99361c46eb4016b9
Stephane Maire <stephanemaire71@gmail.com>
2017-11-06 15:17:29 +0100

    App | user admin bool in store

--- app/containers/App/actions.js
+++ app/containers/App/actions.js
@@ -1,9 +1,16 @@
 
 import {
+  SET_USER_ADMIN,
   INIT_CREDENTIALS,
   UPDATE_CREDENTIALS,
   REQUEST_API
 } from './constants'
+
+export function setUserAdmin () {
+  return {
+    type: SET_USER_ADMIN
+  }
+}
 
 export function initCredentials () {
   return {
--- app/containers/App/userReducer.js
+++ app/containers/App/userReducer.js
@@ -8,14 +8,18 @@ import { fromJS } from 'immutable';
 
 import {
   SET_CREDENTIALS,
+  SET_USER_ADMIN
 } from './constants';
 
 const initialState = fromJS({
-  credentials: null
+  credentials: null,
+  admin: false
 });
 
 function userReducer(state = initialState, action) {
   switch (action.type) {
+    case SET_USER_ADMIN:
+      return state.set('admin', true)
     case SET_CREDENTIALS:
       return state.set('credentials', action.credentials)
     default:
