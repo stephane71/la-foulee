e70d19c4af9325b50a223ed20e7c4ca2072e6b8d
Stephane Maire <stephanemaire71@gmail.com>
2017-11-09 16:20:17 +0100

    Stride | catch PATCH_STRIDE

--- app/containers/Stride/saga.js
+++ app/containers/Stride/saga.js
@@ -1,5 +1,7 @@
 import { takeLatest, put, call } from 'redux-saga/effects';
 
+import { PATCH_STRIDE } from 'containers/Admin/constants'
+
 import { LOAD_STRIDE } from './constants';
 import { setStride } from './actions';
 
@@ -8,7 +10,16 @@ function* loadStride ({ api, data }) {
   yield put(setStride(res.data))
 }
 
+function* patchStride ({ api, strideID, data }) {
+  try {
+    yield call(api.strideStrideIDPatch, { strideID }, data)
+  } catch (e) {
+    console.log('Error on patchStride');
+    console.log(e);
+  }
+}
 
 export default function* defaultSaga() {
   yield takeLatest(LOAD_STRIDE, loadStride)
+  yield takeLatest(PATCH_STRIDE, patchStride)
 }
