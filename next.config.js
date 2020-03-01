18801331b2c913b014d400c168bfb2614d3d8b7a
stephane71 <stephanemaire71@gmail.com>
2020-03-01 12:14:04 +0100

    NextJS config | target serverless

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
-    IDENTITY_POOL_ID: 'eu-west-1:e2689f7c-5ee1-4170-aaac-459223b37007',
-    API_KEY: 'gIyBDAanKl3O3rGFX203W2aS31MbF9yfLT5U2a5g'
+    IDENTITY_POOL_ID: "eu-west-1:e2689f7c-5ee1-4170-aaac-459223b37007",
+    API_KEY: "gIyBDAanKl3O3rGFX203W2aS31MbF9yfLT5U2a5g"
   },
-}
+  target: "serverless"
+};
