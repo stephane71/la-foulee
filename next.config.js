be565cfd4623a3fa40cfda9012f4f99e04d75bec
stephane71 <stephanemaire71@gmail.com>
2020-03-14 17:32:02 +0100

    Next config | require dotenv & replace variables values with .env values

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+require("dotenv").config({ path: "./.env" });
+
 module.exports = {
   env: {
-    IDENTITY_POOL_ID: "eu-west-1:e2689f7c-5ee1-4170-aaac-459223b37007",
-    API_KEY: "gIyBDAanKl3O3rGFX203W2aS31MbF9yfLT5U2a5g"
+    IDENTITY_POOL_ID: process.env.IDENTITY_POOL_ID,
+    API_KEY: process.env.API_KEY
   },
   target: "serverless"
 };
