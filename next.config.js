7b07ae5c47acd2190148257a84058e7fc87aa984
stephane71 <stephanemaire71@gmail.com>
2020-12-19 15:47:15 +0100

    Clean repo

--- next.config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config({ path: "./.env" });
-
-module.exports = {
-  env: {
-    IDENTITY_POOL_ID: process.env.IDENTITY_POOL_ID,
-    API_KEY: process.env.API_KEY
-  },
-  target: "serverless"
-};
