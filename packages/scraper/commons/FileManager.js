da0cd3e695b9a4c74f14632cc54b5cf82dd9c941
stephane71 <stephanemaire71@gmail.com>
2021-01-13 14:54:16 +0100

    Scraper | FileManager.js: run Prettier

--- packages/scraper/commons/FileManager.js
+++ packages/scraper/commons/FileManager.js
@@ -2,19 +2,19 @@ const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 module.exports = class FileManager {
   static uploadToBucket(fileName, data, bucket) {
     const params = {
-      Bucket: bucket,
       Key: fileName,
-      Body: data
+      Body: data,
+      Bucket: bucket,
     };
 
     return new Promise((resolve) => {
-      s3.upload(params, function(s3Err, data) {
+      s3.upload(params, function (s3Err, data) {
         if (s3Err) throw s3Err;
         console.log(`File uploaded successfully at ${data.Location}`);
         resolve();
@@ -25,11 +25,11 @@ module.exports = class FileManager {
   static getFile(key, bucket) {
     const params = {
       Bucket: bucket,
-      Key: key
+      Key: key,
     };
 
     return new Promise((resolve) => {
-      s3.getObject(params, function(s3Err, data) {
+      s3.getObject(params, function (s3Err, data) {
         if (s3Err) throw s3Err;
         // data.Body is a Buffer.
         // see: https://docs.aws.amazon.com/fr_fr/AWSJavaScriptSDK/latest/AWS/S3.html#getObject-property
