5bdf25524e3061597c48f86326367c704a398e7c
stephane71 <stephanemaire71@gmail.com>
2021-01-13 14:55:57 +0100

    Scraper | download-sources: handle upload data to S3 bucket

--- packages/scraper/services/download-sources/index.js
+++ packages/scraper/services/download-sources/index.js
@@ -1,21 +1,41 @@
 const middy = require("@middy/core");
 const httpErrorHandler = require("@middy/http-error-handler");
 const validator = require("@middy/validator");
+const createError = require("http-errors");
 const isFFADepartmentKeyword = require("../../AJVKeywords/isFFADepartment");
+const FileManager = require("../../commons/FileManager");
 const { input: inputSchema } = require("./schemas");
 const downloadDepartmentPage = require("./downloadFFADepartmentPage");
 
 /**
  * Download source page from FFA website
- * @param {object} event
- * @param {object} event.pathParameters
+ * @param {Object} event
+ * @param {Object} event.pathParameters
  *
  */
 async function downloadSource(event) {
   const { year, dep } = event.pathParameters;
 
-  const data = await downloadDepartmentPage(year, dep);
-  console.log(data);
+  // 1. Download document
+  let data = "";
+  try {
+    data = await downloadDepartmentPage(year, dep);
+  } catch (e) {
+    return new createError.NotFound("This page could not be downloaded");
+  }
+
+  // 2. Store document in S3
+  try {
+    await FileManager.uploadToBucket(
+      `${dep}.html`,
+      data,
+      process.env.S3_DESTINATION
+    );
+  } catch (e) {
+    return new createError.InternalServerError(
+      "Problem when try to upload to bucket"
+    );
+  }
 
   return {
     statusCode: 200,
