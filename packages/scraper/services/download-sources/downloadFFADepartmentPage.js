b4f40dbbf994cf3227b20b78dc09a8f835b1a370
stephane71 <stephanemaire71@gmail.com>
2021-01-14 12:39:49 +0100

    Scraper | downloadFFADepartmentPage.js: name the function

--- packages/scraper/services/download-sources/downloadFFADepartmentPage.js
+++ packages/scraper/services/download-sources/downloadFFADepartmentPage.js
@@ -1,11 +1,7 @@
 const axios = require("axios");
 const getSourceURL = require("./getSourceURL");
 
-const BASE_URL = "http://bases.athle.com/asp.net/liste.aspx";
-const getSourceURL = (dep, year = "2019") =>
-  `${BASE_URL}?frmpostback=true&frmbase=calendrier&frmmode=1&frmespace=0&frmsaison=${year}&frmtype1=Hors+Stade&frmtype2=&frmtype3=&frmtype4=&frmniveau=&frmniveaulab=&frmligue=&frmdepartement=${dep}&frmepreuve=&frmdate_j1=&frmdate_m1=&frmdate_a1=&frmdate_j2=&frmdate_m2=&frmdate_a2=`;
-
-module.exports = async function (year, dep) {
+module.exports = async function downloadFFADepartmentPage(year, dep) {
   if (!year || !dep) {
     throw {
       type: "MissingArguments",
