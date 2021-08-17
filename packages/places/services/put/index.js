ad8353210560209acede4be5cd630aca10a0f808
stephane71 <stephanemaire71@gmail.com>
2021-08-17 20:36:08 +0200

    Places | handler : add httpJsonBodyParser middleware && update function

--- packages/places/services/put/index.js
+++ packages/places/services/put/index.js
@@ -1,11 +1,22 @@
 const middy = require("@middy/core");
 const httpErrorHandler = require("@middy/http-error-handler");
+const httpJsonBodyParser = require("@middy/http-json-body-parser");
 const validator = require("@middy/validator");
 // const createError = require("http-errors");
 const inputSchema = require("./schema");
 
-function put() {}
+function put(event) {
+  const { body, pathParameters } = event;
+  const { slug } = pathParameters;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ body, slug }),
+    headers: { "Access-Control-Allow-Origin": "*" },
+  };
+}
 
 module.exports.handler = middy(put)
+  .use(httpJsonBodyParser())
   .use(validator({ inputSchema }))
   .use(httpErrorHandler());
