a3b359a32026f43e6f0cc777228453c3814ef8b7
stephane71 <stephanemaire71@gmail.com>
2021-08-29 13:01:28 +0200

    Places | services:delete: handle delete a place && errors

--- packages/places/services/delete/index.js
+++ packages/places/services/delete/index.js
@@ -1,10 +1,26 @@
 const middy = require("@middy/core");
 const httpErrorHandler = require("@middy/http-error-handler");
 const validator = require("@middy/validator");
-// const createError = require("http-errors");
+const createError = require("http-errors");
 const inputSchema = require("./schema");
+const PlacesTable = require("../PlacesTable");
 
-function deleteItem() {}
+const placesTable = new PlacesTable();
+
+async function deleteItem(event) {
+  const { slug } = event.pathParameters;
+
+  try {
+    await placesTable.deletePlace(slug);
+  } catch (e) {
+    return new createError(e.statusCode);
+  }
+
+  return {
+    statusCode: 200,
+    headers: { "Access-Control-Allow-Origin": "*" },
+  };
+}
 
 module.exports.handler = middy(deleteItem)
   .use(validator({ inputSchema }))
