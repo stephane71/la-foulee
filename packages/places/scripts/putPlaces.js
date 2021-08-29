b1cf214a2d6bcd313c19eaaa47a3aa59d5fc5c0e
stephane71 <stephanemaire71@gmail.com>
2021-08-29 19:15:28 +0200

    Places | scripts:putPlaces : add a break after 5 batch requests

--- packages/places/scripts/putPlaces.js
+++ packages/places/scripts/putPlaces.js
@@ -5,13 +5,19 @@ const apigClient = apigClientFactory.newClient({
   invokeUrl: "https://miifwj1qk7.execute-api.eu-west-3.amazonaws.com/dev",
 });
 
-module.exports = function putPlaces(places) {
-  return places.reduce(async (prev, place) => {
-    await prev;
-    await pause(1000);
+const BREAK = 1000;
+const BATCH = 5;
 
-    console.log("put place", place.slug);
-    return await apigClient
+module.exports = function putPlaces(places) {
+  return places.reduce(async (prev, place, i) => {
+    await prev;
+    if (!(i % BATCH)) {
+      console.log("---- break ----");
+      await pause(BREAK);
+    }
+
+    console.log("put place", place.slug, i);
+    return apigClient
       .invokeApi({ slug: place.slug }, "/{slug}", "PUT", {}, place)
       .catch(function (result) {
         console.error(result);
