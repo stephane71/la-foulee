8570d3d9d2d9465e850d12c9e73d378fe31e6b02
stephane71 <stephanemaire71@gmail.com>
2021-08-30 20:08:31 +0200

    Places | put : schema : add body description

--- packages/places/services/put/schema.js
+++ packages/places/services/put/schema.js
@@ -2,11 +2,19 @@ const crudPathParameters = require("../pathParameters.schema");
 
 module.exports = {
   type: "object",
-  required: ["pathParameters"],
+  required: ["pathParameters", "body"],
   properties: {
     pathParameters: { ...crudPathParameters },
     body: {
       type: "object",
+      required: ["slug", "county", "name", "code", "type"],
+      properties: {
+        slug: { type: "string" },
+        county: { type: "string" },
+        name: { type: "string" },
+        code: { type: "string" },
+        type: { type: "string" },
+      },
     },
   },
 };
