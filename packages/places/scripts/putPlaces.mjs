e25ae9db62bf17de31ef2ccc887542ecdf4bcea9
stephane71 <stephanemaire71@gmail.com>
2021-09-08 08:29:53 +0200

    Places | putPlaces: update batch strategy

--- packages/places/scripts/putPlaces.mjs
+++ packages/places/scripts/putPlaces.mjs
@@ -1,38 +1,50 @@
-import { default as apigClientFactory } from "aws-api-gateway-client";
+import apigClientFactory from "aws-api-gateway-client";
 import { pause } from "@la-foulee/utils";
 
 const API_ID = "nskcrq0b2m";
 const API_REGION = "eu-west-3";
-const apigClient = apigClientFactory.newClient({
+
+const apigClient = apigClientFactory.default.newClient({
   invokeUrl: `https://${API_ID}.execute-api.${API_REGION}.amazonaws.com/dev`,
 });
 
-const BREAK = 1000;
+const BREAK = 10000;
 const BATCH = 10;
 
-let batch = [];
+function batchReq(placesSlice) {
+  console.log(placesSlice);
 
-function putPlaces(places) {
-  return places.reduce(async (prev, place, i) => {
-    if (!(i % BATCH)) {
-      console.log("---- break ----");
-      await Promise.all(batch);
-      await pause(BREAK);
+  return placesSlice.map((place) => {
+    const { slug, county } = place;
+    console.log("put place", county, slug);
 
-      batch = [];
-    } else {
-      batch.push(prev);
+    return apigClient.invokeApi(
+      { county, slug },
+      "/{county}/{slug}",
+      "PUT",
+      {},
+      place
+    );
+  });
+}
+
+async function putPlaces(places) {
+  for (let i = 0; i < places.length; i++) {
+    const start = i * BATCH;
+    const end = (i + 1) * BATCH;
+
+    if (start > places.length) {
+      return Promise.resolve();
     }
 
-    const { slug, county } = place;
-    console.log("put place", place.slug, i);
+    console.log("batch from ", start, "to", end);
 
-    return apigClient
-      .invokeApi({ county, slug }, "/{county}/{slug}", "PUT", {}, place)
-      .catch(function (result) {
-        console.error(result);
-      });
-  }, Promise.resolve());
+    const batch = batchReq(places.slice(start, end));
+    await Promise.all(batch);
+
+    console.log("-- break");
+    await pause(BREAK);
+  }
 }
 
 export default putPlaces;
