8f908ba41e92363eb1d683acb5560e7e47688c17
stephane71 <stephanemaire71@gmail.com>
2021-09-17 08:08:17 +0200

    Scraper | remove legacy tests

--- packages/scraper/services/update-event-location/tests/getEventLocation.test.js
+++
@@ -1,7 +0,0 @@
-const getEventLocation = require("../getEventLocation");
-
-describe("scraper:update-event-location:getEventLocation", () => {
-    test("", () => {
-        expect(true).toEqual(false);
-    });
-});
