542102111eca7821de5f2c82a8353f3513bb9815
stephane71 <stephanemaire71@gmail.com>
2021-09-20 18:32:27 +0200

    Scraper | extract-events : update schema

--- packages/scraper/services/extract-events/schemas.js
+++ packages/scraper/services/extract-events/schemas.js
@@ -1,6 +1,14 @@
 const schema = {
   type: "object",
   required: ["Records"],
+  properties: {
+    Records: {
+      type: "array",
+      items: {
+        type: "object",
+      },
+    },
+  },
 };
 
 export default schema;
